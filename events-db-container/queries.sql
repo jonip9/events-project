e3978f88e0c4aaa542dfe9ffc976d3ae9d775b25
Joni Pössi <joni.possi@gmail.com>
2023-07-10 20:06:57 +0300

    db: Modify queries

--- events-db-container/queries.sql
+++ events-db-container/queries.sql
@@ -1,9 +1,12 @@
+SET timezone TO 'UTC';
 DROP TABLE IF EXISTS event;
 CREATE TABLE event (
-  event_id text PRIMARY KEY,
-  name text NOT NULL,
-  description text,
-  start_date timestamp,
-  duration interval,
-  repeat interval
+  event_id UUID DEFAULT gen_random_uuid() PRIMARY KEY,
+  summary TEXT NOT NULL,
+  descr TEXT,
+  dtstamp TIMESTAMP WITH TIME ZONE NOT NULL,
+  dtstart TIMESTAMP WITH TIME ZONE,
+  dtend TIMESTAMP WITH TIME ZONE,
+  duration INTERVAL,
 );
+INSERT INTO event (summary, descr, dtstamp, dtstart, dtend, duration) VALUES ('event1', 'my descr', '2023-05-01', '2023-05-13', '2023-05-15', 'P7D');
