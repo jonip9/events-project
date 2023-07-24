18264bedb883461b22ef1d45c0509b78be35b398
Joni Pössi <joni.possi@gmail.com>
2023-07-24 19:57:34 +0300

    server: Update id name in SQL statement

--- events-ring-server/src/events_ring_server/resolvers.clj
+++ events-ring-server/src/events_ring_server/resolvers.clj
@@ -6,7 +6,7 @@
   (jdbc/execute! ds-opts ["SELECT * FROM event"]))
 
 (defn event-by-id [context args value]
-  (jdbc/execute-one! ds-opts ["SELECT * FROM event WHERE id = ?" (:id args)]))
+  (jdbc/execute-one! ds-opts ["SELECT * FROM event WHERE event_id = ?" (:id args)]))
 
 (def resolvers-map {:query/all-events all-events
                     :query/event-by-id event-by-id})
