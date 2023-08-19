db3fa5e7203545d12c8cc17a928aec8f51047924
Joni Pössi <joni.possi@gmail.com>
2023-08-19 03:26:21 +0300

    server: Handle PGInterval type conversion

--- events-ring-server/src/events_ring_server/resolvers.clj
+++ events-ring-server/src/events_ring_server/resolvers.clj
@@ -1,17 +1,22 @@
 (ns events-ring-server.resolvers
   (:import [org.postgresql.util PGInterval]
-           [java.sql PreparedStatement])
   (:require [events-ring-server.database :refer [ds-opts]]
+           [java.sql PreparedStatement]
+           [java.time Duration])
             [next.jdbc :as jdbc]
             [next.jdbc.date-time]
-            [next.jdbc.prepare :as p]))
+            [next.jdbc.prepare :as p]
+            [next.jdbc.result-set :as rs]))
 
 (defn ->pg-interval
+  "Takes a Duration instance and converts it into a PGInterval
+   instance where the interval is created as a number of seconds."
   [^java.time.Duration duration]
   (doto (PGInterval.)
-    (.setValue (.toString duration))))
+    (.setSeconds (.getSeconds duration))))
 
 (extend-protocol p/SettableParameter
+  ;; Convert durations to PGIntervals before inserting into db.
   java.time.Duration
   (set-parameter [^java.time.Duration v
                   ^PreparedStatement s
@@ -20,9 +25,25 @@
 
 (defn all-events [context args value]
   (jdbc/execute! ds-opts ["SELECT * FROM event"]))
+(defn <-pg-interval
+  "Takes a PGInterval instance and converts it into a Duration
+   instance. Ignore sub-second units."
+  [^org.postgresql.util.PGInterval interval]
+  (-> Duration/ZERO
+      (.plusSeconds (.getSeconds interval))
+      (.plusMinutes (.getMinutes interval))
+      (.plusHours (.getHours interval))
+      (.plusDays (.getDays interval))))
 
 (defn event-by-id [context args value]
   (jdbc/execute-one! ds-opts ["SELECT * FROM event WHERE event_id = ?" (:id args)]))
+(extend-protocol rs/ReadableColumn
+  ;; Convert PGIntervals back to durations.
+  org.postgresql.util.PGInterval
+  (read-column-by-label [^org.postgresql.util.PGInterval v _]
+    (<-pg-interval v))
+  (read-column-by-index [^org.postgresql.util.PGInterval v _2 _3]
+    (<-pg-interval v)))
 
 (defn insert-event [context args value]
   (let [{summary :summary
