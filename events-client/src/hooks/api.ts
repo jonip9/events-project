6cee0ea2c93b46d55b2eb7ac4d4e22c6649d9d25
Joni Pössi <joni.possi@gmail.com>
2023-09-02 04:19:40 +0300

    client: Fetch events from API

---
+++ events-client/src/hooks/api.ts
@@ -0,0 +1,42 @@
+import { useState, useEffect } from 'preact/hooks';
+
+export function useGetEvents(id?: string) {
+  const [isLoading, setIsLoading] = useState(true);
+  const [data, setData] = useState();
+  const [error, setError] = useState();
+  const base = 'http://localhost:3000/api/events';
+
+  useEffect(() => {
+    const abortController = new AbortController();
+    const url = id
+      ? `${base}/${id}`
+      : base;
+    const props: RequestInit = {
+      method: 'GET',
+      headers: {
+	'Content-Type': 'application/json'
+      },
+      signal: abortController.signal
+    };
+
+    const getData = async () => {
+      try {
+	setIsLoading(true);
+        const res = await fetch(url, props);
+        const json = await res.json();
+	setData(json);
+      } catch (e) {
+	setError(e);
+      } finally {
+	setIsLoading(false);
+      }
+    };
+    getData();
+
+    return () => {
+      abortController.abort();
+    }
+  }, [id]);
+
+  return { isLoading, data, error };
+}
--- events-client/src/pages/Home.tsx
+++ events-client/src/pages/Home.tsx
@@ -1,99 +1,51 @@
-import { useState, useEffect } from 'preact/hooks';
+import { useGetEvents } from '../hooks/api';
 
 interface Event {
-  event_id: number;
+  event_id: string;
   summary: string;
   descr: string;
   dtstamp: string;
   dtstart: string;
   dtend: string;
-  duration: string;
+  duration: number;
 }
 
 interface EventItem {
-  event_id: number;
+  event_id: string;
   name: string;
   description: string;
   timestamp: Date;
 }
 
 export function Home() {
-  const [eventData, setEventData] = useState<Event[]>([]);
+  const { isLoading, data, error } = useGetEvents();
 
-  useEffect(() => {
-    setEventData([
-      {
-        event_id: 0,
-        summary: 'Event 1',
-        descr: 'My description',
-        dtstamp: '2023-08-24T10:00:00',
-        dtstart: '2023-08-24T10:00:00',
-        dtend: '2023-12-24T00:00:00',
-        duration: '1 months 7 days'
-      },
-      {
-        event_id: 1,
-        summary: 'Event 2',
-        descr: 'Another description',
-        dtstamp: '2023-10-14T14:30:00',
-        dtstart: '2023-10-14T14:30:00',
-        dtend: '2024-01-14T00:00:00',
-        duration: '2 days'
-      }
-    ])
-  }, []);
-
-  function getInterval(interval: string[], start: Date, end: Date): number {
-    if (interval.length === 0) return end.getTime() - start.getTime();
-    const kind = interval.pop();
-    const amount = Number.parseInt(interval.pop());
-
-    switch (kind) {
-      case 'days':
-        end.setDate(end.getDate() + amount);
-        break;
-      case 'months':
-        end.setMonth(end.getMonth() + amount);
-        break;
-      case 'years':
-        end.setFullYear(end.getFullYear() + amount);
-        break;
-    }
-
-    return getInterval(interval, start, end);
-  }
-
-  function generateEvents(event: Event, ival: number, offset: number): EventItem[] {
+  function generateEvents(event: Event): EventItem[] {
+    const d = new Date();
     let curr = new Date(event.dtstart);
     const end = new Date(event.dtend);
     const arr = [];
 
     while (curr <= end) {
       const tstamp = new Date(curr);
-      tstamp.setMinutes(tstamp.getMinutes() - (offset - tstamp.getTimezoneOffset()));
+
+      tstamp.setMinutes(tstamp.getMinutes() - (d.getTimezoneOffset() - tstamp.getTimezoneOffset()));
       arr.push({
         event_id: event.event_id,
         name: event.summary,
         description: event.descr,
         timestamp: tstamp
       });
-
-      curr = new Date(curr.getTime() + ival);
+      curr.setTime(curr.getTime() + (event.duration * 1000))
     }
 
     return arr;
   }
 
   function combineEvents(events: Event[]): EventItem[] {
-    const d = new Date();
     const allEvents = events
       .map((item) => {
-        const ival = getInterval(
-          item.duration.split(' '),
-          d,
-          new Date()
-        );
-        return generateEvents(item, ival, d.getTimezoneOffset());
+        return generateEvents(item);
       })
       .flat()
       .sort((a, b) => a.timestamp.getTime() - b.timestamp.getTime());
@@ -113,11 +65,11 @@ export function Home() {
           </div>
           <div class="eventsList">
             <div>
-              {eventData.length > 0 && (
-                combineEvents(eventData)
+              {data && (
+                combineEvents(data)
                   .map((item) => {
                     return (
-                      <div key={item.event_id + item.timestamp.getTime()}>
+                      <div key={`${item.event_id}${item.timestamp.getTime()}`}>
                         <div>{item.name}</div>
                         <div>{item.description}</div>
                         <div>{item.timestamp.toString()}</div>
